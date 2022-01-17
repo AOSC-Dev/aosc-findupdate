24ab9ad021e775cccec153d082587ff0935fe4b1
liushuyu <liushuyu011@gmail.com>
2022-01-16 20:41:21 -0700

    deps: update dependencies ...
    ... also pin `quote` version to 1.0.10 to avoid issues

--- src/cli.rs
+++ src/cli.rs
@@ -1,36 +1,36 @@
-use clap::{crate_version, App, Arg};
+use clap::{App, Arg};
 
-pub fn build_cli() -> App<'static, 'static> {
+pub fn build_cli() -> App<'static> {
     App::new("aosc-findupdate")
-        .version(crate_version!())
+        .version(env!("CARGO_PKG_VERSION"))
         .about("Find updated packages in the abbs tree")
         .arg(
-            Arg::with_name("DRY_RUN")
+            Arg::new("DRY_RUN")
                 .long("dry-run")
                 .takes_value(false)
                 .help("Do not update the files in the abbs tree"),
         )
         .arg(
-            Arg::with_name("LOG")
-                .short("l")
+            Arg::new("LOG")
+                .short('l')
                 .takes_value(true)
                 .help("Log updated packages to a file"),
         )
         .arg(
-            Arg::with_name("FILE")
-                .short("f")
+            Arg::new("FILE")
+                .short('f')
                 .takes_value(true)
                 .help("Path to a list of packages to be updated"),
         )
         .arg(
-            Arg::with_name("INCLUDE")
-                .short("i")
+            Arg::new("INCLUDE")
+                .short('i')
                 .takes_value(true)
                 .help("Use regular expression to filter which package to update"),
         )
         .arg(
-            Arg::with_name("DIR")
-                .short("d")
+            Arg::new("DIR")
+                .short('d')
                 .takes_value(true)
                 .help("Specify the directory to the abbs tree"),
         )
