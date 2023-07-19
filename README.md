ab7dd2a1957748586e185e663bf13419db94737b
Cyan <cyanoxygen@aosc.io>
2023-07-19 10:38:52 +0800

    aosc-findupdate: add compliance mode
    - This mode aims to modify the returned version strings, to comply with
      the AOSC OS Package Styling Manual.
    - This mode is not enabled by default, unless '-c' switch is enabled.
    - Users are required to double check the resulting spec file if
      compliance mode is enabled.

--- README.md
+++ README.md
@@ -14,17 +14,17 @@ This is a small utility that helps AOSC developers to find package updates in th
 ## Usage
 
 ```
-USAGE:
-    aosc-findupdate [OPTIONS]
-
-FLAGS:
-    -h, --help       Prints help information
-    -V, --version    Prints version information
+USAGE: aosc-findupdate [OPTIONS]
 
 OPTIONS:
-    -d <DIR>            Specify the directory to the abbs tree
-    -f <FILE>           Path to a list of packages to be updated
-    -i <INCLUDE>        Use regular expression to filter which package to update
+      --dry-run     Do not update the files in the abbs tree
+  -l <LOG>          Log updated packages to a file
+  -f <FILE>         Path to a list of packages to be updated
+  -i <INCLUDE>      Use regular expression to filter which package to update
+  -d <DIR>          Specify the directory to the abbs tree
+  -c                Modify version strings to comply with the AOSC Package Styling Manual
+  -h, --help        Print help
+  -V, --version     Print version
 ```
 
 ### Suggested Usages
@@ -38,3 +38,12 @@ Example: Do a Rust rebuild survey: `aosc-findupdate -f groups/rust-rebuilds`
 Example: Do a general survey with all packages matching the pattern "extra-d*": `aosc-findupdate -i 'extra-d.+'`
 
 (Note that the pattern is in **Regex syntax**, not bash globbing syntax!)
+
+
+### AOSC OS Package Styling Manual compliance
+
+AOSC Find Update does not comply with the [AOSC OS Package Styling Manual](https://wiki.aosc.io/developer/packaging/package-styling-manual/#versioning-variables) by default, unless the `-c` switch is enabled.
+
+With the `-c` switch enabled, AOSC Find Update will transform the returned version number automatically in accordance with the Styling Manual. However, the regexes to transform version numbers are not strict enough to prevent unwanted modifications.
+
+Always double check your `spec` file if you have enabled the `-c` switch.
---
+++ src/filter.rs
@@ -0,0 +1,149 @@
+//! This module modifies the version string to comply with the [AOSC Package Styling Manual](https://wiki.aosc.io/developer/packaging/package-styling-manual/#versioning-variables).
+use regex::{self, Regex};
+/// Matches version strings with letter notation.
+///
+/// e.g. `1.2.3-p5`
+///
+/// The dash will be removed..
+const REGEX_LETTER_NOTATION: &str = r"^\d+(?:\.\d+)+[-_~+^][a-z]\d+$";
+/// Matches version strings with dashes
+///
+/// e.g. `2023-05-07`
+///
+/// We replace the dashes with dots.
+const REGEX_DASHES: &str = r"^\d+(?:-\d+)+$";
+/// Matches version strings with underscores
+///
+/// e.g. `10_2`
+///
+/// We replace the underscores with dots.
+const REGEX_UNDERSCORES: &str = r"^\d+(?:_[0-9a-zA-Z]+)+$";
+/// Matches version strings with release types (rc, beta, alpha)
+///
+/// e.g. `4.5-rc1`
+///
+/// We replace the symbol with a plus sign (`+`).
+const REGEX_RELEASE_TYPES: &str = r"^\d+(?:\.\d+)+[-_~^]*(?:rc|beta|alpha)\d*$";
+/// Matches version strings with revisions.
+///
+/// e.g. `5.4.3-2`
+///
+/// We replace the dash with tilde (`~`).
+const REGEX_REVISION: &str = r"^\d+(?:\.\d+)+(?:-\d+)+$";
+/// Trait for str
+///
+/// So one can modify the version string with version_str.comply_with_aosc().
+pub trait VersionStr {
+    fn compily_with_aosc(&self) -> String;
+}
+
+#[derive(PartialEq, Debug, Clone, Copy)]
+enum VersioningType {
+    Normal,
+    LetterNotation,
+    Dashes,
+    Underscores,
+    ReleaseTypes,
+    Revision,
+}
+
+fn version_type(version_string: &str) -> VersioningType {
+    let matcher_letter_notation = Regex::new(REGEX_LETTER_NOTATION).unwrap();
+    let matcher_dashes = Regex::new(REGEX_DASHES).unwrap();
+    let matcher_underscores = Regex::new(REGEX_UNDERSCORES).unwrap();
+    let matcher_release_types = Regex::new(REGEX_RELEASE_TYPES).unwrap();
+    let matcher_revision = Regex::new(REGEX_REVISION).unwrap();
+    if matcher_release_types.is_match(&version_string) {
+        return VersioningType::ReleaseTypes
+    }
+    if matcher_dashes.is_match(&version_string) {
+        return VersioningType::Dashes
+    }
+    if matcher_underscores.is_match(&version_string) {
+        return VersioningType::Underscores
+    }
+    if matcher_letter_notation.is_match(&version_string) {
+        return VersioningType::LetterNotation
+    }
+    if matcher_revision.is_match(version_string) {
+        return VersioningType::Revision
+    }
+    VersioningType::Normal
+}
+
+impl VersionStr for str {
+    /// Modifies the version string to comply with the [AOSC Package Styling Manual](https://wiki.aosc.io/developer/packaging/package-styling-manual/#versioning-variables).
+    /// The searching regexes are strict enough to not to modify the part it is not supposed to do.
+    fn compily_with_aosc(&self) -> String {
+        let mut filtered_ver = self.to_lowercase();
+        let versioning_type = version_type(&filtered_ver);
+        match versioning_type {
+            VersioningType::Normal => {
+                // Nothing to do.
+            }
+            VersioningType::LetterNotation => {
+                let replacer = Regex::new(r"[-_~+^]").unwrap();
+                filtered_ver = replacer.replace_all(&filtered_ver.as_str(), "").to_string();
+            }
+            VersioningType::Dashes => {
+                let replacer = Regex::new(r"[-_]").unwrap();
+                filtered_ver = replacer.replace_all(&filtered_ver.as_str(), ".").to_string();
+            }
+            VersioningType::Underscores => {
+                let replacer = Regex::new(r"[-_]").unwrap();
+                filtered_ver = replacer.replace_all(&filtered_ver.as_str(), ".").to_string();
+            }
+            VersioningType::ReleaseTypes => {
+                let replacer = Regex::new(r"[-+~^]*((?:rc|alpha|beta)\S+)").unwrap();
+                filtered_ver = replacer.replace_all(&filtered_ver.as_str(), "~$1").to_string();
+            }
+            VersioningType::Revision => {
+                let replacer = Regex::new(r"[-_~+^]").unwrap();
+                filtered_ver = replacer.replace_all(&filtered_ver.to_string(), "+").to_string();
+            }
+        }
+        filtered_ver
+    }
+}
+
+
+#[test]
+fn test_version_type() {
+    let normal_version_str = &"1.2.3";
+    let version_str_with_letter_notation = &"1.2.3-p6";
+    let version_str_with_dashes = &"2023-07-18";
+    let version_str_with_rev = &"6.4-20230718";
+    let version_str_with_rel = &"5.3-56";
+    let version_str_with_rc = &"0.9.1rc1";
+    let version_str_with_rc_and_dash = &"2.16-rc1";
+    let version_str_with_alpha = &"3.0-alpha5";
+
+    assert_eq!(version_type(normal_version_str), VersioningType::Normal);
+    assert_eq!(version_type(version_str_with_letter_notation), VersioningType::LetterNotation);
+    assert_eq!(version_type(version_str_with_dashes), VersioningType::Dashes);
+    assert_eq!(version_type(version_str_with_rev), VersioningType::Revision);
+    assert_eq!(version_type(version_str_with_rel), VersioningType::Revision);
+    assert_eq!(version_type(version_str_with_alpha), VersioningType::ReleaseTypes);
+    assert_eq!(version_type(version_str_with_rc), VersioningType::ReleaseTypes);
+    assert_eq!(version_type(version_str_with_rc_and_dash), VersioningType::ReleaseTypes);
+}
+
+#[test]
+fn test_comply_with_aosc() {
+    let normal_version_str = &"1.2.3";
+    let version_str_with_letter_notation = &"1.2.3-p6";
+    let version_str_with_dashes = &"2023-07-18";
+    let version_str_with_rev = &"6.4-20230718";
+    let version_str_with_rel = &"5.3-56";
+    let version_str_with_rc = &"0.9.1rc1";
+    let version_str_with_rc_and_dash = &"2.16-rc1";
+    let version_str_with_alpha = &"3.0-alpha5";
+    assert_eq!(normal_version_str.compily_with_aosc(), String::from(normal_version_str.to_owned()));
+    assert_eq!(version_str_with_letter_notation.compily_with_aosc(), String::from("1.2.3p6"));
+    assert_eq!(version_str_with_dashes.compily_with_aosc(), String::from("2023.07.18"));
+    assert_eq!(version_str_with_rev.compily_with_aosc(), String::from("6.4+20230718"));
+    assert_eq!(version_str_with_rel.compily_with_aosc(), String::from("5.3+56"));
+    assert_eq!(version_str_with_rc.compily_with_aosc(), String::from("0.9.1~rc1"));
+    assert_eq!(version_str_with_rc_and_dash.compily_with_aosc(), String::from("2.16~rc1"));
+    assert_eq!(version_str_with_alpha.compily_with_aosc(), String::from("3.0~alpha5"));
+}
