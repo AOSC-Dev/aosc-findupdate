AOSC-Dev/aosc-findupdate

commit e3d95d3b1ec8fa5a69f2c6727be4e63e829a3d63

deps: update dependencies ...
... Also adapt winnow 0.5

src/parser.rs
@@ -8,38 +8,36 @@ use std::{
 };
 use winnow::{
     ascii::alphanumeric1,
-    branch::alt,
-    bytes::{tag, take_until0},
-    combinator::repeat,
-    sequence::{separated_pair, terminated},
-    IResult, Parser,
+    combinator::{alt, repeat, separated_pair, terminated},
+    token::{tag, take_until0},
+    PResult, Parser,
 };
 
 type Context = HashMap<String, String>;
 
 const CONFIG_SEPARATOR: &str = "::";
 
-fn take_type(input: &str) -> IResult<&str, &str> {
+fn take_type<'a>(input: &mut &'a str) -> PResult<&'a str> {
     take_until0(CONFIG_SEPARATOR).parse_next(input)
 }
 
-fn kv_key_inner(input: &str) -> IResult<&str, ()> {
+fn kv_key_inner<'a>(input: &mut &'a str) -> PResult<()> {
     repeat(1.., alt((alphanumeric1, tag("_")))).parse_next(input)
 }
 
-fn kv_key(input: &str) -> IResult<&str, &str> {
+fn kv_key<'a>(input: &mut &'a str) -> PResult<&'a str> {
     kv_key_inner.recognize().parse_next(input)
 }
 
-fn kv_pair(input: &str) -> IResult<&str, (&str, &str)> {
+fn kv_pair<'a>(input: &mut &'a str) -> PResult<(&'a str, &'a str)> {
     separated_pair(kv_key, tag("="), take_until0(";")).parse_next(input)
 }
 
-fn kv_pairs(input: &str) -> IResult<&str, Vec<(&str, &str)>> {
+fn kv_pairs<'a>(input: &mut &'a str) -> PResult<Vec<(&'a str, &'a str)>> {
     repeat(1.., terminated(kv_pair, tag(";"))).parse_next(input)
 }
 
-fn config_line(input: &str) -> IResult<&str, (&str, Vec<(&str, &str)>)> {
+fn config_line<'a>(input: &mut &'a str) -> PResult<(&'a str, Vec<(&'a str, &'a str)>)> {
     separated_pair(take_type, tag(CONFIG_SEPARATOR), kv_pairs).parse_next(input)
 }
 
@@ -54,13 +52,13 @@ pub(crate) fn parse_spec<P: AsRef<Path>>(spec: P) -> Result<Context> {
     Ok(context)
 }
 
-pub(crate) fn parse_check_update(content: &str) -> Result<Context> {
+pub(crate) fn parse_check_update(content: &mut &str) -> Result<Context> {
     let parsed = config_line(content).map_err(|err| anyhow!("Invalid config line: {}", err))?;
     let mut context = HashMap::new();
     let config = parsed.1;
-    context.insert("type".to_string(), config.0.to_string());
+    context.insert("type".to_string(), content.to_string());
 
-    for (k, v) in config.1 {
+    for (k, v) in config {
         context.insert(k.to_string(), v.to_string());
     }
 
@@ -129,24 +127,34 @@ pub(crate) fn expand_package_list<P: AsRef<Path>, I: IntoIterator<Item = P>>(
 
 #[test]
 fn test_take_type() {
-    let test = "test::1";
-    assert_eq!(take_type(test), Ok(("::1", "test")));
+    let test = &mut "test::1";
+    let res = take_type(test);
+
+    assert_eq!(res, Ok("test"));
+    assert_eq!(test, &mut "::1");
 }
 
 #[test]
 fn test_kv_key() {
-    let test = "a_b123";
-    assert_eq!(kv_key(test), Ok(("", "a_b123")));
+    let test = &mut "a_b123";
+    let res = kv_key(test);
+    assert_eq!(res, Ok("a_b123"));
+    assert_eq!(test, &mut "");
 }
 
 #[test]
 fn test_kv() {
-    let test = "a=b;";
-    assert_eq!(kv_pair(test), Ok((";", ("a", "b"))));
+    let test = &mut "a=b;";
+    let res = kv_pair(test);
+    assert_eq!(res, Ok(("a", "b")));
+    assert_eq!(test, &mut ";")
 }
 
 #[test]
 fn test_kv_pairs() {
-    let test = "a=b;b=d;";
-    assert_eq!(kv_pairs(test), Ok(("", vec![("a", "b"), ("b", "d")])));
+    let test = &mut "a=b;b=d;";
+    let res = kv_pairs(test);
+
+    assert_eq!(res, Ok(vec![("a", "b"), ("b", "d")]));
+    assert_eq!(test, &mut "");
 }

Cargo.toml
@@ -12,7 +12,7 @@ anyhow = "1.0"
 serde = "1.0"
 version-compare = "0.1"
 percent-encoding = "2.2"
-winnow = "0.4"
+winnow = "0.5"
 clap = "^4"
 abbs-meta-apml = { git = "https://github.com/AOSC-Dev/abbs-meta-rs", rev = "d75c0abd6cf2b5016452b036ddfbd144e6d611ed" }
 walkdir = "2.3"
@@ -20,7 +20,7 @@ log = "0.4"
 env_logger = "0.10"
 aho-corasick = "1.0"
 owo-colors = "3.5"
-sailfish = "0.6"
+sailfish = "0.8"
 
 [profile.release]
 lto = true

src/checker/git.rs
@@ -7,37 +7,35 @@ use reqwest::blocking::Client;
 use reqwest::header::USER_AGENT;
 use winnow::{
     ascii::{multispace1, not_line_ending, space1},
-    combinator::repeat,
-    sequence::{separated_pair, terminated},
+    combinator::{repeat, separated_pair, terminated},
     stream::AsChar,
     token::take_while,
-    IResult, Parser,
+    PResult, Parser,
 };
 
 const SIMULATED_GIT_VERSION: &str = "2.31.1";
 
 // parser-combinators for parsing Git on-wire format
-fn first_tuple(input: &[u8]) -> IResult<&[u8], &[u8]> {
+fn first_tuple<'a>(input: &mut &'a [u8]) -> PResult<&'a [u8]> {
     take_while(1.., |c: u8| c.is_hex_digit() || c == b'#').parse_next(input)
 }
 
-fn kv_pair(input: &[u8]) -> IResult<&[u8], (&[u8], &[u8])> {
+fn kv_pair<'a>(input: &mut &'a [u8]) -> PResult<(&'a [u8], &'a [u8])> {
     separated_pair(first_tuple, space1, not_line_ending).parse_next(input)
 }
 
-fn single_line(input: &[u8]) -> IResult<&[u8], (&[u8], &[u8])> {
+fn single_line<'a>(input: &mut &'a [u8]) -> PResult<(&'a [u8], &'a [u8])> {
     terminated(kv_pair, multispace1).parse_next(input)
 }
 
-fn parse_git_manifest(input: &[u8]) -> IResult<&[u8], Vec<(&[u8], &[u8])>> {
+fn parse_git_manifest<'a>(input: &mut &'a [u8]) -> PResult<Vec<(&'a [u8], &'a [u8])>> {
     repeat(1.., single_line).parse_next(input)
 }
 // end of parser-combinators
 
-fn collect_git_tags(input: &[u8]) -> Result<Vec<&str>> {
+fn collect_git_tags<'a>(input: &mut &'a [u8]) -> Result<Vec<&'a str>> {
     let tuples = parse_git_manifest(input).map_err(|e| anyhow!("Parser error: {:?}", e))?;
     let tags: Vec<_> = tuples
-        .1
         .iter()
         .filter_map(|x| {
             if x.1.ends_with(&b"^{}"[..]) {
@@ -78,7 +76,7 @@ impl UpdateChecker for GitChecker {
             .send()?;
         resp.error_for_status_ref()?;
         let body = resp.bytes()?;
-        let mut tags = collect_git_tags(&body)?
+        let mut tags = collect_git_tags(&mut body.to_vec().as_ref())?
             .into_iter()
             .map(|x| x.to_string())
             .collect::<Vec<_>>();
@@ -96,70 +94,64 @@ impl UpdateChecker for GitChecker {
 
 #[test]
 fn first_tuple_test() {
-    let test = &b"001e# "[..];
-    assert_eq!(first_tuple(test), Ok((&b" "[..], &b"001e#"[..])));
+    let test = &mut &b"001e# "[..];
+    assert_eq!(first_tuple(test), Ok(&b"001e#"[..]));
+    assert_eq!(test, &mut &b" "[..]);
 }
 
 #[test]
 fn kv_test() {
     // blob descriptor
-    let test = &b"003fdb358a2993be0e0aa3864ed3290105dd4a544c35 refs/heads/avx512\n"[..];
+    let test = &mut &b"003fdb358a2993be0e0aa3864ed3290105dd4a544c35 refs/heads/avx512\n"[..];
     assert_eq!(
         kv_pair(test),
         Ok((
-            &b"\n"[..],
-            (
-                &b"003fdb358a2993be0e0aa3864ed3290105dd4a544c35"[..],
-                &b"refs/heads/avx512"[..]
-            )
+            &b"003fdb358a2993be0e0aa3864ed3290105dd4a544c35"[..],
+            &b"refs/heads/avx512"[..]
         ))
     );
+    assert_eq!(test, &mut &b"\n"[..]);
     // service descriptor
-    let test = &b"001e# service=git-upload-pack\n"[..];
+    let test = &mut &b"001e# service=git-upload-pack\n"[..];
     assert_eq!(
         kv_pair(test),
-        Ok((&b"\n"[..], (&b"001e#"[..], &b"service=git-upload-pack"[..])))
+        Ok((&b"001e#"[..], &b"service=git-upload-pack"[..]))
     );
+    assert_eq!(test, &mut &b"\n"[..]);
     // capability descriptor
-    let test = &b"000000fe68e3802b238b964900acac9422a70e295482243f HEAD\x00multi_ack no-done symref=HEAD:refs/heads/master agent=git/2.11.4.GIT\n"[..];
+    let test = &mut &b"000000fe68e3802b238b964900acac9422a70e295482243f HEAD\x00multi_ack no-done symref=HEAD:refs/heads/master agent=git/2.11.4.GIT\n"[..];
     assert_eq!(
         kv_pair(test),
         Ok((
-            &b"\n"[..],
-            (
-                &b"000000fe68e3802b238b964900acac9422a70e295482243f"[..],
-                &b"HEAD\x00multi_ack no-done symref=HEAD:refs/heads/master agent=git/2.11.4.GIT"[..]
-            )
+            &b"000000fe68e3802b238b964900acac9422a70e295482243f"[..],
+            &b"HEAD\x00multi_ack no-done symref=HEAD:refs/heads/master agent=git/2.11.4.GIT"[..]
         ))
     );
+    assert_eq!(test, &mut &b"\n"[..],);
 }
 
 #[test]
 fn test_multiline() {
-    let test = &b"01234abc heads\n12345bcd tags\n"[..];
+    let test = &mut &b"01234abc heads\n12345bcd tags\n"[..];
     assert_eq!(
         parse_git_manifest(test),
-        Ok((
-            &b""[..],
-            vec![
-                (&b"01234abc"[..], &b"heads"[..]),
-                (&b"12345bcd"[..], &b"tags"[..]),
-            ]
-        ))
+        Ok(vec![
+            (&b"01234abc"[..], &b"heads"[..]),
+            (&b"12345bcd"[..], &b"tags"[..]),
+        ])
     );
+    assert_eq!(test, &mut &b""[..]);
     // with caps and trailer
-    let test = &b"001e# service=git-upload-pack\n01234abc heads\n12345bcd tags\n0000"[..];
+    let test = &mut &b"001e# service=git-upload-pack\n01234abc heads\n12345bcd tags\n0000"[..];
     assert_eq!(
         parse_git_manifest(test),
-        Ok((
-            &b"0000"[..],
-            vec![
-                (&b"001e#"[..], &b"service=git-upload-pack"[..]),
-                (&b"01234abc"[..], &b"heads"[..]),
-                (&b"12345bcd"[..], &b"tags"[..]),
-            ]
-        ))
+        Ok(vec![
+            (&b"001e#"[..], &b"service=git-upload-pack"[..]),
+            (&b"01234abc"[..], &b"heads"[..]),
+            (&b"12345bcd"[..], &b"tags"[..]),
+        ])
     );
+    assert_eq!(test, &mut &b"0000"[..]);
 }
 
 #[test]

src/main.rs
@@ -115,7 +115,7 @@ fn check_update_worker<P: AsRef<Path>>(
     })?;
     let mut warnings = Vec::new();
     let config_line = config_line.to_owned() + ";"; // compensate for the parser quirk
-    let config = parser::parse_check_update(&config_line)?;
+    let config = parser::parse_check_update(&mut config_line.as_str())?;
     let new_version = checker::check_update(&config, client)?;
     let new_version = new_version.trim();
     let new_version = new_version.strip_prefix('v').unwrap_or(new_version);

commit 6e135c000ac7a95904ca4dcf82f82223d549fc91

parse: fix "Unknown type" errors
Appears to be broken since https://github.com/AOSC-Dev/aosc-findupdate/commit/e3d95d3b1ec8fa5a69f2c6727be4e63e829a3d63

src/parser.rs
@@ -56,7 +56,7 @@ pub(crate) fn parse_check_update(content: &mut &str) -> Result<Context> {
     let parsed = config_line(content).map_err(|err| anyhow!("Invalid config line: {}", err))?;
     let mut context = HashMap::new();
     let config = parsed.1;
-    context.insert("type".to_string(), content.to_string());
+    context.insert("type".to_string(), parsed.0.to_string());
 
     for (k, v) in config {
         context.insert(k.to_string(), v.to_string());